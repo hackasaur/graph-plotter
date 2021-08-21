6eeb29a342d5f1d2d47c5775370b95780f3112a7
Manik Raina <manikraina1096gmail.com>
2021-08-22 02:36:49 +0530

    f(x) coords on mousemove and Math. equation

--- graph.js
+++ graph.js
@@ -29,15 +29,30 @@ function draw() {
             ctx.fillText(text, coords[0], coords[1])
         }
 
-        const checkEquation = (fOfX, characters) => {
-            for (let character of fOfX) {
-                if (characters.includes(character)) {
+        const checkEquation = (fOfX_string, characters) => {
+            for (let i = 0; i < fOfX_string.length; i++) {
+                if (characters.includes(fOfX_string[i])) {
                     continue
                 }
+                else if (fOfX_string[i] === 'M') {
+                    if (fOfX_string.slice(i, i + 5) === 'Math.') {
+                        let p = i + 5
+                        for (p; p < fOfX_string.length; p++) {
+                            if (characters.includes(fOfX_string[p])) {
+                                i = p + 1
+                                break
+                            }
+                        }
+                        continue
+                    }
+                    else {
+                        return false
+                    }
+                }
                 else {
-                    console.log(character)
                     return false
                 }
+
             }
             return true
         }
@@ -74,39 +89,53 @@ function draw() {
             let startX = Math.floor(-axisLength / properties.unitPixels)
             let endX = Math.ceil(axisLength / properties.unitPixels)
 
-            return {
-                draw: () => {
-                    if (properties.unitPixels <= 0) {
-                        throw 'unitPixels cannot be <= 0'
+            const generateCurvePoints = () => {
+                if (properties.unitPixels <= 0) {
+                    throw 'unitPixels cannot be <= 0'
+                }
+                let pointsArray = []
+                let dx = 1 / properties.unitPixels
+                let startPoint = cartesianToCanvasCoords(createPoint(startX, properties.fOfX(startX)), properties.originInCanvas, properties.unitPixels)
+                let canvasX = startPoint[0]
+                let canvasY = startPoint[1]
+                for (let x = startX; x <= endX - dx; x += dx) {
+                    canvasX = canvasX + 1
+                    canvasY = properties.originInCanvas[1] - properties.unitPixels * properties.fOfX(x + dx)
+                    if (canvasX < canvas.width && canvasY < canvas.height) {
+                        pointsArray.push(createPoint(canvasX, canvasY))
                     }
+                    else {
+                        continue
+                    }
+                }
+                return pointsArray
+            }
+
+            let pointsArray = generateCurvePoints()
+
+            return {
+                getPointsArray: () => {
+                    return pointsArray
+                },
+                draw: () => {
                     //make lines that trace coordinates of the function in an interval
-                    let dx = 1 / properties.unitPixels
                     ctx.strokeStyle = "white"
                     ctx.lineWidth = lineWidth
                     ctx.lineJoin = 'round'
-                    let startPoint = cartesianToCanvasCoords(createPoint(startX, properties.fOfX(startX)), properties.originInCanvas, properties.unitPixels)
                     ctx.beginPath()
-                    let canvasX = startPoint[0]
-                    let canvasY = startPoint[1]
-                    ctx.moveTo(canvasX, canvasY)
-                    for (let x = startX; x <= endX - dx; x += dx) {
-                        canvasX = canvasX + 1
-                        canvasY = properties.originInCanvas[1] - properties.unitPixels * properties.fOfX(x + dx)
-                        if (canvasX < canvas.width && canvasY < canvas.height) {
-                            ctx.lineTo(canvasX, canvasY)
-                        }
-                        else {
-                            ctx.moveTo(canvasX, canvasY)
-                        }
+                    ctx.moveTo(pointsArray[0][0], pointsArray[0][1])
+                    for (let i = 0; i < pointsArray.length; i++) {
+                        ctx.lineTo(pointsArray[i][0], pointsArray[i][1])
                     }
                     ctx.stroke()
                 },
                 getProperties: () => {
                     return properties
                 },
-                updateStartEnd: () => {
+                update: () => {
                     startX = Math.floor(-axisLength / properties.unitPixels)
                     endX = Math.ceil(axisLength / properties.unitPixels)
+                    pointsArray = generateCurvePoints()
                 }
             }
         }
@@ -121,7 +150,7 @@ function draw() {
                     if (properties.unitPixels <= 0) {
                         throw 'unitPixels cannot be <= 0'
                     }
-                    ctx.strokeStyle = "purple"
+                    ctx.strokeStyle = "orange"
                     ctx.lineWidth = 1
                     //y axis
                     ctx.beginPath()
@@ -135,8 +164,9 @@ function draw() {
                     let originY = properties.originInCanvas[1]
 
                     ctx.beginPath()
+                    //hatchMarks for x axis
                     numberOfHatchMarks = Math.floor(properties.axisLength / properties.unitPixels)
-                    for (let hatchMarkIndex = 0; hatchMarkIndex <= numberOfHatchMarks; hatchMarkIndex++) {
+                    for (let hatchMarkIndex = 1; hatchMarkIndex <= numberOfHatchMarks; hatchMarkIndex++) {
                         let hatchMarkOffset = hatchMarkIndex * properties.unitPixels
                         ctx.moveTo(originX + hatchMarkOffset, originY - properties.hatchMarkLength)
                         ctx.lineTo(originX + hatchMarkOffset, originY + properties.hatchMarkLength)
@@ -144,7 +174,7 @@ function draw() {
                         ctx.lineTo(originX - hatchMarkOffset, originY + properties.hatchMarkLength)
                     }
                     //hatchMarks for y axis
-                    for (let hatchMarkIndex = 0; hatchMarkIndex <= numberOfHatchMarks; hatchMarkIndex++) {
+                    for (let hatchMarkIndex = 1; hatchMarkIndex <= numberOfHatchMarks; hatchMarkIndex++) {
                         let hatchMarkOffset = hatchMarkIndex * properties.unitPixels
                         ctx.moveTo(originX + properties.hatchMarkLength, originY + hatchMarkOffset)
                         ctx.lineTo(originX - properties.hatchMarkLength, originY + hatchMarkOffset)
@@ -163,16 +193,13 @@ function draw() {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             axis.draw()
             curve.draw()
-            printOnCanvas(ctx,
-                `x:${Math.round(mouseCoordsInCartesian[0] * 100) / 100},y:${Math.round(mouseCoordsInCartesian[1] * 100) / 100}`,
-                mouseCoords,
-                { fontStyle: 'Fira Mono', fontColor: 'grey', fontSize: '20' })
+            graphImage = ctx.getImageData(origin[0] - axisLength, origin[1] - axisLength, 2 * axisLength, 2 * axisLength)
         }
 
         let origin = createPoint(Math.floor(canvas.width / 2), Math.floor(canvas.height / 2))
         let unit = 40
         let axisLength = Math.floor(canvas.height / 2) - 5
-        let allowedCharacters = ['x', '+', '-', '*', '/']
+        let allowedCharacters = ['x', '+', '-', '*', '/', '.', '(', ')', ' ']
         let zoomRate = 5
         for (let i = 0; i < 10; i++) {
             allowedCharacters.push(`${i}`)
@@ -183,10 +210,11 @@ function draw() {
 
         let theAxis = axis(ctx, axisLength, origin, unit)
         let theAxisProps = theAxis.getProperties()
-        let theCurve = curve(ctx, (x) => { return 3 * Math.sin(x) }, 2, origin, unit, axisLength)
+        let theCurve = curve(ctx, fOfX, 2, origin, unit, axisLength)
         let theCurveProps = theCurve.getProperties()
         let mouseCoordsInCartesian = createPoint(0, -10)
         let mouseCoords = createPoint(0, -10)
+        let graphImage
 
         renderGraph(theAxis, theCurve)
 
@@ -197,7 +225,8 @@ function draw() {
             console.log(unit)
             theCurveProps.unitPixels = unit
             theAxisProps.unitPixels = unit
-            theCurve.updateStartEnd()
+            theCurve.update()
+            renderGraph(theAxis, theCurve)
         })
 
         document.getElementById('zoom out').addEventListener('click', () => {
@@ -207,7 +236,8 @@ function draw() {
             console.log(unit)
             theCurveProps.unitPixels = unit
             theAxisProps.unitPixels = unit
-            theCurve.updateStartEnd()
+            theCurve.update()
+            renderGraph(theAxis, theCurve)
         })
 
         document.getElementById('submit equation').addEventListener('click', () => {
@@ -215,23 +245,57 @@ function draw() {
             fOfX_string = document.getElementById('f of x').value
             fOfX = returnFOfX(fOfX_string, allowedCharacters)
             theCurveProps.fOfX = fOfX
+            theCurve.update()
+            renderGraph(theAxis, theCurve)
         })
 
         canvas.addEventListener('mousemove', (event) => {
             mouseCoords = createPoint(event.x - canvas.offsetLeft, event.y - canvas.offsetTop)
             mouseCoordsInCartesian = canvasToCartestianCoords(mouseCoords, origin, unit)
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.putImageData(graphImage, origin[0] - axisLength, origin[1] - axisLength)
+
+            let y = fOfX(mouseCoordsInCartesian[0])
+            ctx.fillStyle = 'blue'
+            ctx.beginPath()
+            ctx.arc(mouseCoords[0], origin[1] - unit * y, 3, 0, 2 * Math.PI)
+            ctx.fill()
+            printOnCanvas(ctx,
+                `x:${Math.round(mouseCoordsInCartesian[0] * 100) / 100}, y:${Math.round((y) * 100) / 100}`,
+                createPoint(mouseCoords[0], origin[1] - unit * y),
+                { fontStyle: 'Fira Mono', fontColor: 'grey', fontSize: '20' })
         })
 
-        let frameNumber = 0
 
-        async function main() {
-            if (frameNumber <= 5000) {
-                window.requestAnimationFrame(main)
+        function animateCurve(curve) {
+            let curvePointsArray = curve.getPointsArray()
+            console.log(curvePointsArray)
+            let index = 0
+            let chunk = 5
+            ctx.strokeStyle = "white"
+            ctx.lineWidth = 2
+            ctx.lineJoin = 'round'
+            ctx.beginPath()
+            ctx.moveTo(curvePointsArray[0][0], curvePointsArray[0][1])
+
+            function loop() {
+                for (let i = 1; i <= chunk; i++) {
+                    ctx.lineTo(curvePointsArray[index + i][0], curvePointsArray[index + i][1])
+                }
+                ctx.stroke()
+                if (curvePointsArray.length - index > chunk) {
+                    index += chunk
+                }
+                else {
+                    index += 1
+                    chunk = curvePointsArray.length - index
+                }
+
+                if (index <= curvePointsArray.length) {
+                    window.requestAnimationFrame(loop)
+                }
             }
-            renderGraph(theAxis, theCurve)
-            frameNumber++
-            console.log('frame #')
+            loop()
         }
-        main()
     }
 }
